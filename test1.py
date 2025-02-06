057bc845346d09fc9ba540a57120b7ccb9206b50
denismaggior8 <denis.maggiorotto@sunnyvale.it>
2025-02-06 15:54:30 +0100

    test

---
+++ test1.py
@@ -0,0 +1,27 @@
+import sys
+import os 
+print(sys.path)
+sys.path.insert(0, '/Users/denismaggiorotto/.micropython/lib')
+print(sys.path)
+import mip
+mip.install("github:denismaggior8/micropython-enigma-python")
+
+import logging
+import copy
+from enigmapython.EnigmaB_A133RotorI import EnigmaB_A133RotorI
+from enigmapython.EnigmaB_A133Etw import EnigmaB_A133Etw
+from enigmapython.EnigmaB_A133 import EnigmaB_A133
+from enigmapython.ReflectorUKW_EnigmaB_A133 import ReflectorUKW_EnigmaB_A133
+
+rotor1 = EnigmaB_A133RotorI(ring=0,position=0)
+rotor2 = EnigmaB_A133RotorI(ring=0,position=0)
+rotor3 = EnigmaB_A133RotorI(ring=0,position=0)
+reflector = ReflectorUKW_EnigmaB_A133()
+etw = EnigmaB_A133Etw()
+
+
+# Setup Enigma B machine
+enigma = EnigmaB_A133(rotor3, rotor2, rotor1, reflector, etw, True)
+
+print(enigma.input_string("denis"))
+
